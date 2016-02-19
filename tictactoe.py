712bc60ad189ea047ac75f1dcbc58f90e50193a9
lokesh-t <lokesh.t43@live.com>
2016-02-19 19:49:13 +0530

    changed the layout style
    added dictionary type

--- tictactoe.py
+++ tictactoe.py
@@ -4,15 +4,34 @@ Created on Tue Oct 20 22:20:05 2015
 
 @author: admin
 """
-board={'tl':' ','tm':' ','tr':' ','ml':' ','mm':' ','mr':' ','ll':' ','lm':' ','lr':' '}
+board={'tr':' ','tm':' ','tl':' ','mr':' ','mm':' ','ml':' ','lr':' ','lm':' ','ll':' '}
 def display():
-    print " | | "
-    print "_____"
-    print " | | "
-    print "_____"
-    print " | | "
+    print  board['tr'],
+    print  "|",
+    print  board['tm'],
+    print "|",
+    print  board['tl']
+    bottom_lines()
+    print board['mr'],
+    print "|",
+    print board['mm'],
+    print "|",
+    print board['ml']
+    bottom_lines()
+    print board['lr'],
+    print "|",
+    print board['lm'],
+    print "|",
+    print board['ll']
+def bottom_lines():
+    print '_',
+    print '_',
+    print '_',
+    print '_',
+    print '_'
 def values():
     for i in board.itervalues():
         print i
 print 'hi welcome to tictac toe'
 display()
+print 'enter your move'
