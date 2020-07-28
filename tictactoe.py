7af3174ea5fe0ee9de44cf641dd872123659885b
lokesh <lokesh.t43@live.com>
2020-07-28 23:25:17 +0530

    wrote list comprehensions to remove repeated code and some observed tweaks to improve program

--- tictactoe.py
+++ tictactoe.py
@@ -4,23 +4,28 @@ Created on Tue Oct 20 22:20:05 2015
 
 @author: admin
 """
-valid_moves=1
-valid_token = 0
-board={1:'1',2:'2',3:'3',4:'4',5:'5',6:'6',7:'7',8:'8',9:'9'}
 
-def line_values(i):
-    temp=[]
-    for i in range(i,i+3):
-        temp.append(board[i])
-    print("|".join(temp))
+valid_moves = 1
+valid_token = 0
+board = {1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9'}
+
 
 def display():
-    for i in range(1,10,3):
-        print(board[i],"|",board[i+1],"|",board[i+2])
+    # displays board in cli
+    for i in range(1, 10, 3):
+        print(board[i], "|", board[i+1], "|", board[i+2])
         if i < 7:
             print("---------")
 
-def replace(inp_move,cor_mov):
+def replace(ind,val):
+    #replacing board value with the move
+    board[int(ind)] = val
+    global valid_moves
+    valid_moves += 1
+
+def inp_validation(inp_move, cor_mov):
+    #para : input string and correct token corresponding
+    #       to the player 
     try:
         idx, val = inp_move.split(':')
     except Exception:
@@ -29,13 +34,8 @@ def replace(inp_move,cor_mov):
     else:
         if idx.isnumeric() and 0 < int(idx) <= 9:
             if val == cor_mov:
-                if board[int(idx)] is not 'o':
-                    if board[int(idx)] is not 'x':
-                        board[int(idx)] = val
-                        global valid_moves
-                        valid_moves+=1
-                    else:
-                        print("invalid.the position is already filled")
+                if board[int(idx)] is not 'o' and board[int(idx)] is not 'x':
+                        replace(idx,val)
                 else:
                     print("invalid.the position is already filled")
             else:
@@ -56,73 +56,53 @@ def token_validation(value):
     else:
         print("invalid. enter 'x' or 'o' ")
 
-def check_board(players):
-    player1 = players[1]*3
-    player2 = players[2]*3
-    if "".join([board[1],board[2],board[3]]) == player1:
+def check_board(players_sym):
+    # check if a player won
+    # para : dictionary containing players
+    player1_streak = players_sym[1] * 3
+    player2_streak = players_sym[2] * 3
+    
+    data = list(board.values())
+    columns = [1, 2, 3]
+    rows = [1, 4, 7]
+    total_data_set = []
+    # populate total win possibilities
+    # rows and columns
+    for i in rows:
+        total_data_set.append("".join(data[i-1 : i+2]))
+    for i in columns:
+        total_data_set.append(f'{data[i-1]}{data[i+2]}{data[i+5]}')
+    # diagonals
+    total_data_set.append(f'{data[0]}{data[4]}{data[8]}')
+    total_data_set.append(f'{data[2]}{data[4]}{data[6]}')
+    # if pattern present, print winner
+    if player1_streak in total_data_set:
+        print('Player1 is the winner')
         global valid_moves
         valid_moves = 10
-        print('player1 wins')
-    if "".join([board[1],board[2],board[3]]) == player2:
+        return
+    if player2_streak in total_data_set:
+        print('player2 is the winner')
         valid_moves = 10
-        print('player2 wins')
-    if "".join([board[4],board[5],board[6]]) == player1:
-        valid_moves = 10
-        print('player1 wins')
-    if "".join([board[4],board[5],board[6]]) == player2:
-        valid_moves = 10
-        print('player2 wins')
-    if "".join([board[7],board[8],board[9]]) == player1:
-        valid_moves = 10
-        print('player1 wins')
-    if "".join([board[7],board[8],board[9]]) == player2:
-        valid_moves = 10
-        print('player2 wins')
-    if "".join([board[1],board[5],board[9]]) == player1:
-        valid_moves = 10
-        print('player1 wins')
-    if "".join([board[1],board[5],board[9]]) == player2:
-        valid_moves = 10
-        print('player2 wins')
-    if "".join([board[3],board[5],board[7]]) == player1:
-        valid_moves = 10
-        print('player1 wins')
-    if "".join([board[3],board[5],board[7]]) == player2:
-        valid_moves = 10
-        print('player2 wins')
-    if "".join([board[1],board[4],board[7]]) == player1:
-        valid_moves = 10
-        print('player1 wins')
-    if "".join([board[1],board[4],board[7]]) == player2:
-        valid_moves = 10
-        print('player2 wins')
-    if "".join([board[2],board[6],board[8]]) == player1:
-        valid_moves = 10
-        print('player1 wins')
-    if "".join([board[2],board[6],board[8]]) == player2:
-        valid_moves = 10
-        print('player2 wins')
-    if "".join([board[3],board[7],board[9]]) == player1:
-        valid_moves = 10
-        print('player1 wins')
-    if "".join([board[3],board[7],board[9]]) == player2:
-        valid_moves = 10
-        print('player2 wins')
-#line_values(1)
+        return
+
+
 print('hi welcome to tictac toe')
 print('this is a 2 player game')
 players={1:'',2:''}
+#selecting player1 token
 while valid_token < 1:
     token_validation(input('player1 choose either "x" or "o"'))
+#main logic
 while valid_moves < 10:
     display()
     print('your move should be in format of "number:token" ')
     if valid_moves%2 !=0:
         print(f"player1 turn. please enter 'number:{players[1]}'")
-        replace(input('enter your move: '),players[1])
+        inp_validation(input('enter your move: '),players[1])
     else:
         print(f"player2 turn. please enter 'number:{players[2]}'")
-        replace(input('enter your move: '),players[2])
+        inp_validation(input('enter your move: '),players[2])
     if valid_moves > 4:
         check_board(players)
 print("game finished")
